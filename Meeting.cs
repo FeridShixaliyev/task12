255e0903f16cb3badc1890bd0ca3029e50ba3afd
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-04 11:32:49 +0400

    task2

---
+++ Meeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp41
+{
+    class Meeting
+    {
+        public string Name { get; set; }
+        public DateTime FromDate;
+        public DateTime ToDate;
+    }
+}
