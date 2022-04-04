6aef82e87be20a1884220070fdd32ba5270b4d77
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-04 11:33:10 +0400

    task2

---
+++ MeetingSchedule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp41
+{
+    class MeetingSchedule
+    {
+        List<Meeting> meetings = new List<Meeting>();
+        
+        
+        public string SetMeeting(string name,DateTime fromdate,DateTime todate,DateTime meetdate)//meetdate yoxlanacag vaxtdi
+        {
+            if (meetdate > fromdate && meetdate < todate)
+            {
+                throw new Exception("bu araliqda meeting var!!");
+            }
+            else
+            {
+                string Names= meetings.Add(name);
+            }
+        }
+        public int FindMeetingsCount(DateTime meet,DateTime fromDate)
+        {
+            int count = 0;
+            if (meet < fromDate)
+            {
+                count++;
+            }
+            return count;
+        }
+        public bool IsExistsMeetingByName(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                if (str == meetings.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<string> GetExistMeetings(string name)
+        {
+            List<string> findname=new List<string>();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                if (name == meetings.Name)
+                {
+                    findname.Add(name);
+                }
+            }
+            return null;
+        }
+    }
+}
